eb7b7bf8b4f81012e8b0b2dd35bde307cbda4cd2
Tracy Falba <tfalba@mac.com>
2020-12-01 09:30:30 -0500

    proper search for artist and added media player

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
 
-const urlApi = 'https://itunes.apple.com/search?term='
-const audioPlayer = document.querySelector('#audio-player')
+const urlApi = 'https://itunes.apple.com/search?media=music&attribute=artistTerm&term='
+const audioPlayer = document.querySelector('#media-player')
+const body = document.querySelector('body')
+
+
+//https://itunes.apple.com/search?term=john%2Blegend
+
 function getTracks (artist) {
-  fetch(urlApi + artist)
+  const url = urlApi + artist
+  // console.log(`${url}&media=music&entity=musicArtist`)
+  fetch (url)
     .then(res => res.json())
     .then(user => {
       for (const result of user.results) {
         console.log(result.collectionName)
         renderTrack(result)
       }
+      console.log(urlApi+artist)
       // debugger
       // for (let song of user) {
       //   console.log(song.collectionName)
       // }
     }
     )
+    // body.classList.remove('background-img')
 }
-
+// function getAudio (obj) {
+//   // a fetch request here for the selected item -- this may be a reason to post locally??
+//   // so that can get from local?? 
+// }
 const cardHolder = document.querySelector('#card-holder')
-cardHolder.addEventListener('click', function(event) {
-  audioPlayer.innerHTML = ``
+cardHolder.addEventListener('click', function (event) {
+  // getAudio (objTarget) -- don't need this with below
+// (figure out if this will pull the assigned value)
+  const audioValue = event.target.dataset.target
+  debugger
+  audioPlayer.innerHTML = `<audio controls autoplay src=${audioValue}></audio>`
 })
+// could asign a value during rendering to be able to pull the fetch url
+// something like =>
+// trackImage.innerHTML = `<img class='image' value=${track.previewUrl} src=${track.artworkUrl100}></img>`
 
 function renderTrack(track) {
   const newCard = document.createElement('div')
@@ -30,13 +49,18 @@ function renderTrack(track) {
   const trackImage = document.createElement('div')
   trackImage.classList.add('image-div')
   newCard.appendChild(trackImage)
+
   const trackTitle = document.createElement('div')
   trackTitle.classList.add('title')
   newCard.appendChild(trackTitle)
 
-  trackImage.innerHTML = `<img class='image' src=${track.artworkUrl100}></img>`
-  trackTitle.innerHTML = track.trackName
+  // const collectionName = document.createElement('div')
+  // collectionName.classList.add('collection')
+  // newCard.appendChild(collectionName)
 
+  trackImage.innerHTML = `<img class='image' data-target=${track.previewUrl} src=${track.artworkUrl100}></img>`
+  trackTitle.innerHTML = track.trackName
+  // collectionName.innerHTML = track.collectionName
 }
 
 const form = document.querySelector('#search-field')
