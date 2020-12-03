d84befc460b7525bbc039163d8ff2e634ea4d188
Tracy Falba <tfalba@mac.com>
2020-12-03 08:58:33 -0500

    cleaned up js file with comments etc

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-
-const urlApiArtist = 'https://itunes.apple.com/search?media=music&attribute=artistTerm&term='
-const urlApiSong = 'https://itunes.apple.com/search?media=music&attribute=songTerm&term='
-const urlApiAlbum = 'https://itunes.apple.com/search?media=music&attribute=albumTerm&term='
-const urlApiAll = 'https://itunes.apple.com/search?media=music&term='
-
-// const urlApiArtist = 'https://itunes-api-proxy.glitch.me/search?media=music&attribute=artistTerm&term='
-// const urlApiSong = 'https://itunes-api-proxy.glitch.me/search?media=music&attribute=songTerm&term='
-// const urlApiAlbum = 'https://itunes-api-proxy.glitch.me/search?media=music&attribute=albumTerm&term='
-// const urlApiAll = 'https://itunes-api-proxy.glitch.me/search?media=music&term='
+/* ------------------------------------------------------------------------------------------------------------------ */
+/*                                          Set elements that will be grabbed                                         */
+/* ------------------------------------------------------------------------------------------------------------------ */
 
 const audioPlayer = document.querySelector('#media-player')
 const cardHolder = document.querySelector('#card-holder')
@@ -16,7 +9,6 @@ const searchTerm = document.querySelector('input')
 const selector = document.querySelector('#selector')
 const sortField = document.querySelector('#sort-field')
 const sortOptions = document.querySelector('#sort-options')
-// let sortKeyword = sortOptions.value
 let originalArray = []
 
 /* ------------------------------------------------------------------------------------------------------------------ */
@@ -25,16 +17,23 @@ let originalArray = []
 
 function runSearch (url) {
   originalArray = []
+  // validateSearch(data)
   fetch (url)
     .then(res => res.json())
     .then(data => {
-      // validateSearch(data)
+
+/* --------------------------------- Push data for each track to an array of objects -------------------------------- */
+
       for (const result of data.results) {
         if (result.collectionName !== undefined) {
           originalArray.push(result)
-          // renderTrack(result)
         }
       }
+
+/* ---------------- Sort each array depending on sort feature criteria -- use callback sort functions --------------- */
+
+/* -------------------------------- Either default or all sets new array as original -------------------------------- */
+
       let newArray = []
       if (sortOptions.value !== 'default') {
         if (sortOptions.value === 'artist') {
@@ -51,8 +50,10 @@ function runSearch (url) {
       } else {
         newArray = originalArray
       }
+
+/* ------------------------------- Now renderTrack over whichever array is appropriate ------------------------------ */
+
       for (let i = 0; i < newArray.length; i++) {
-        // console.log('am i getting to this line?')
         renderTrack(newArray[i])
       }
       if (originalArray.length === 0) {
@@ -63,7 +64,9 @@ function runSearch (url) {
     })
 }
 
-/* ------------------------------- Now renderTrack over whichever array is appropriate ------------------------------ */
+/* ------------ Sets of combinations to set up for sort array based on whichever object key is being used ----------- */
+/* ---------------------------------- Could turn this into a function for all four ---------------------------------- */
+
 function artist (a, b) {
   // Use toUpperCase() to ignore character casing
   const varA = a.artistName.toUpperCase()
@@ -88,7 +91,6 @@ function album (a, b) {
   }
   return comparison
 }
-
 function song (a, b) {
   // Use toUpperCase() to ignore character casing
   const varA = a.trackName.toUpperCase()
@@ -101,7 +103,6 @@ function song (a, b) {
   }
   return comparison
 }
-
 function year (a, b) {
   // Use toUpperCase() to ignore character casing
   const varA = a.releaseDate.toUpperCase()
@@ -113,39 +114,19 @@ function year (a, b) {
     comparison = -1
   }
   return comparison
- }
-// don't need selector as an arg here...
-// function getTracks (keyword) {
+}
 
-// }
 function getTracks (searchWord, selectionType) {
   const url = `https://itunes.apple.com/search?media=music&attribute=${selectionType}&term=${encodeURI(searchWord)}`
   runSearch(url)
-  // if (selector === 'artist') {
-  //   const url = urlApiArtist + encodeURI(keyword)
-  //   console.log('running artist search')
-  //   runSearch(url)
-  // } else if (selector === 'song') {
-  //   const url = urlApiSong + encodeURI(keyword)
-  //   runSearch(url)
-  // } else if (selector === 'album') {
-  //   const url = urlApiAlbum + encodeURI(keyword)
-  //   runSearch(url)
-  // } else if (selector === 'default') {
-  //   const url = urlApiAll + encodeURI(keyword)
-  //   runSearch(url)
-  // }
 }
 
 /* ------------------------------------------------------------------------------------------------------------------ */
 /*                                                  DOM Manipulation                                                  */
 /* ------------------------------------------------------------------------------------------------------------------ */
 
-// change track input to be input from whichever array want to display after sort
-// track is just the originalArray[i]
-
-function renderTrack(track) {
-  // validateInput(track)
+function renderTrack (track) {
+/* ------------------ Place new card in container and then append children to it for track details ------------------ */
 
   const newCard = document.createElement('div')
   newCard.classList.add('card')
@@ -159,8 +140,6 @@ function renderTrack(track) {
   trackTitle.classList.add('title')
   newCard.appendChild(trackTitle)
 
-  /* ---------------------------- save this if want to add collection name or other details --------------------------- */
-
   const collectionName = document.createElement('div')
   collectionName.classList.add('collection')
   newCard.appendChild(collectionName)
@@ -169,34 +148,20 @@ function renderTrack(track) {
   artistName.classList.add('artist-name')
   newCard.appendChild(artistName)
 
-  // debugger
+  /* ------------------------------------------------ Validation checks ----------------------------------------------- */
   if (originalArray.length === 0) {
     cardHolder.innerHTML = 'Your search has returned no results. Please try a new one.'
     console.log('no results')
   } else {
+    /* -------------------------------------------- Else set content of card -------------------------------------------- */
+
     trackImage.innerHTML = `<img class='image' data-target=${track.previewUrl} data-title="${track.trackName}" src=${track.artworkUrl100}></img>`
-
     trackTitle.innerHTML = track.trackName
-
     const trackYear = track.releaseDate.slice(0,4)
     collectionName.innerHTML = `${track.collectionName} (${trackYear})`
     artistName.innerHTML = track.artistName
   }
 }
-
-/* ------------------------------------------------------------------------------------------------------------------ */
-/*                                                Validation Functions                                                */
-/* ------------------------------------------------------------------------------------------------------------------ */
-
-// function validateInput(inputTrack) {
-//   // checks on what input looks like before adding --
-//   if (inputTrack.trackTitle !== undefined) {
-//   }
-// }
-
-// function validateSearch(searchData) {
-//   // checks for search not returning 200
-// }
 
 /* ------------------------------------------------------------------------------------------------------------------ */
 /*                                                   Event Listeners                                                  */
@@ -227,31 +192,12 @@ form.addEventListener('submit', function (event) {
 
   for (let i = 0; i <= 6; i++) {
     if (selector.children[i].checked === true) {
-      console.log('artist is selected')
+      console.log(`${selector.children[i].value} is selected as selector`)
       getTracks(searchTerm.value, selector.children[i].value)
-    }
-    else {
-      console.log('HELP')
+    } else {
+      console.log("SHOULDN'T BE HERE -- NO SELECTOR CHECKED")
     }
   }
-
-  // if (selector.children[0].checked === true) {
-  //   console.log('artist is selected')
-  //   getTracks(searchTerm.value, selector.children[0].value)
-  // } else if (selector.children[2].checked === true) {
-  //   console.log('song is selected')
-  //   getTracks(searchTerm.value, selector.children[2].value)
-  // } else if (selector.children[4].checked === true) {
-  //   console.log('album is selected')
-  //   getTracks(searchTerm.value, selector.children[4].value)
-  // } else {
-  //   console.log('defaulting to all')
-  //   getTracks(searchTerm.value, selector.children[6].value)
-  // }
-  // else return error message???
-
-  //getTracks(searchTerm.value, selector)
-  // searchTerm.value = ''
 })
 
 sortOptions.addEventListener('change', function (event) {
@@ -262,11 +208,9 @@ sortOptions.addEventListener('change', function (event) {
 
   for (let i = 0; i <= 6; i++) {
     if (selector.children[i].checked === true) {
-      // console.log('artist is selected')
       getTracks(searchTerm.value, selector.children[i].value)
-    }
-    else {
-      console.log('HELP')
+    } else {
+      console.log("SHOULDN'T BE HERE -- NO SELECTOR CHECKED FOR -- STUCK IN SORT FUNCTION")
     }
   }
 })
